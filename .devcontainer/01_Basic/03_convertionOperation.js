1c3df661695930bb9073000de841c905b7aa38f9
poojadhanajijadhav <poojajadhav1906@gmail.com>
2025-02-24 19:23:35 +0000

    operation

--- .devcontainer/01_Basic/03_convertionOperation.js
+++ .devcontainer/01_Basic/03_convertionOperation.js
@@ -31,4 +31,49 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// link to read https://tc39.es/ecma262/#sec-type-conversion
+
+//***************************** Operation  *********************************** */
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Pooja"
+let str2 = "Hello"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+"2"); //12
+// console.log(1+2+"2"+2+3); //3223
+
+// console.log(true); //true
+// console.log(+true+false); //1
+// console.log(+true+false); //1
+// console.log(+""); //0
+
+let num1, num2, num3
+num1 = num2 = num3 = 4
+
+let gamecounter = 100
+++gamecounter;
+// gamecounter++;
+
+// above both give same result
+// link to read https://tc39.es/ecma262/#sec-prefix-increment-operator
+console.log(gamecounter);
+
+
