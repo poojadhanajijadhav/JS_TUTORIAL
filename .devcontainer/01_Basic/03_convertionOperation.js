5d2c02c0beb014e5c2bdf936101747e169d46312
poojadhanajijadhav <poojajadhav1906@gmail.com>
2025-02-24 19:04:32 +0000

    converstion of datatype

---
+++ .devcontainer/01_Basic/03_convertionOperation.js
@@ -0,0 +1,34 @@
+let score ="pooja"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// pooja => NaN
+
+let isLoggedIn = ""
+let  boolisloggedIn = Boolean(isLoggedIn)
+
+// console.log(boolisloggedIn)
+
+
+// 1 => true
+// 0 => false
+// "" => false
+// "pooja" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
