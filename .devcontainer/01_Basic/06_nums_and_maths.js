3f78d7b602142517a32ed762ef5d4614e3c6317b
poojadhanajijadhav <poojajadhav1906@gmail.com>
2025-03-05 20:20:47 +0000

    datetime

---
+++ .devcontainer/01_Basic/06_nums_and_maths.js
@@ -0,0 +1,22 @@
+const score = 400;
+const balance = new Number(100);
+console.log(balance);
+const anotherNum = 123.5688;
+console.log(anotherNum.toPrecision(4));
+console.log(anotherNum.toPrecision(3));
+console.log(anotherNum.toPrecision(2));
+console.log(anotherNum.toPrecision(5));
+const longNum = 1000000000;
+console.log(longNum.toLocaleString());
+console.log(longNum.toLocaleString('en-IN'));
+
+
+//****************Maths*********************************** */
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.random());
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.6));
+console.log(Math.min(4, 2,3,6,7));
